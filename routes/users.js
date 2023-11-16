d5fcb53934807c1e0cf0969ffd965cff741a6431
HO Vo Minh Khue <20210334@univ.etud-evry.fr>
2023-11-16 10:01:52 +0100

    delete test

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,5 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.send("respond with a resource");
 });
-router.get("/cool", (req, res, next) => {
-  res.send("You're so cool");
-});
 
 module.exports = router;
