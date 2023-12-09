04ab23a79776ea4699fd4b00f751927dea409c69
HO Vo Minh Khue <20210334@univ.etud-evry.fr>
2023-12-09 20:31:55 +0100

    set up rate limiter

--- app.js
+++ app.js
@@ -58,6 +58,15 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 //set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
