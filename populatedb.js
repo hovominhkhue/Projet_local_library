05a6ace22ada8b04d99e2bb89312965e22464906
HO Vo Minh Khue <20210334@univ.etud-evry.fr>
2024-07-01 22:15:56 +0200

    corrige populatedb.json

--- populatedb.js
+++ populatedb.js
@@ -1,23 +1,19 @@
 #! /usr/bin/env node
 
-console.log(
-  'mongodb+srv://hovominhkhue:Khueho1206@cluster0.89wkobs.mongodb.net/local_library?retryWrites=true&w=majority'
-);
+// Load environment variables
+require('dotenv').config();
 
-const moment = require('moment');
-
-// Get arguments passed on command line
-const userArgs = process.argv.slice(2);
+const moment = require('moment'); // Currently unused, consider removing if not needed
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const Book = require("./models/book.js");
 const Author = require("./models/author.js");
 const books = [];
 const authors = [];
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const mongoDB = userArgs[0];
+// Use the MongoDB URI from environment variables
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 
@@ -31,17 +27,12 @@ async function main() {
   mongoose.connection.close();
 }
 
-// We pass the index to the ...Create functions so that, for example,
-// genre[0] will always be the Fantasy genre, regardless of the order
-// in which the elements of promise.all's argument complete.
-
 async function authorCreate(index, first_name, family_name, d_birth, d_death) {
   const authordetail = { firstName: first_name, lastName: family_name };
   if (d_birth != false) authordetail.dateOfBirth = d_birth;
   if (d_death != false) authordetail.dateOfDeath = d_death;
 
   const author = new Author(authordetail);
-
   await author.save();
   authors[index] = author;
   console.log(`Added author: ${first_name} ${family_name}`);
@@ -81,41 +72,6 @@ async function createBooks() {
       "9781473211896",
       authors[0]
     ),
-    bookCreate(1,
-      "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-      "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-      "9788401352836",
-      authors[0]
-    ),
-    bookCreate(2,
-      "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-      "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-      "9780756411336",
-      authors[0]
-    ),
-    bookCreate(3,
-      "Apes and Angels",
-      "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-      "9780765379528",
-      authors[1]
-    ),
-    bookCreate(4,
-      "Death Wave",
-      "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-      "9780765379504",
-      authors[1]
-    ),
-    bookCreate(5,
-      "Test Book 1",
-      "Summary of test book 1",
-      "ISBN111111",
-      authors[4]
-    ),
-    bookCreate(6,
-      "Test Book 2",
-      "Summary of test book 2",
-      "ISBN222222",
-      authors[4]
-    ),
+    // Add additional book creates as necessary
   ]);
 }
